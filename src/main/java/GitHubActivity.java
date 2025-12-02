409b864627d41be90c080b3214be14adead428de
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2025-12-02 05:14:05 +0100

    Can now list all events from a github user

--- src/main/java/GitHubActivity.java
+++ src/main/java/GitHubActivity.java
@@ -1,16 +1,31 @@
-import com.google.gson.Gson;
-
+import com.google.gson.*;
 import java.io.IOException;
 import java.net.URISyntaxException;
+import java.net.http.HttpResponse;
+
 
 public class GitHubActivity {
     static void main(String[] args) {
         String username = "olujobii";
-        Gson gson = new Gson();
+
+        if(args.length == 0){
+            System.out.println("No username is specified");
+            return;
+        }
+
+        if(args.length != 1){
+            System.out.println("Wrong usage");
+        }
         APIRepository apiRepository = new APIRepository(username);
+        HttpResponse<String> httpResponse;
 
         try{
-            apiRepository.fetchData();
+            httpResponse = apiRepository.fetchData();
+            if(httpResponse.statusCode() == 404){
+                System.out.println("Username not found");
+                return;
+            }
+            listEvents(httpResponse);
         }
         catch(URISyntaxException e)
         {
@@ -19,6 +34,53 @@ public class GitHubActivity {
         }
         catch(InterruptedException | IOException e){
             System.out.println("An error occurred while fetching data");
+        }
+    }
+
+    static void listEvents(HttpResponse<String> httpResponse){
+        JsonArray jsonArray = JsonParser.parseString(httpResponse.body()).getAsJsonArray();
+        if(jsonArray.isEmpty())
+        {
+            System.out.println("No events available from this user");
+            return;
+        }
+
+        for(var elements : jsonArray){
+            JsonObject jsonObject = elements.getAsJsonObject();
+            String type = jsonObject.get("type").getAsString(); //Get type of event
+            String repoUrl = jsonObject.get("repo").getAsJsonObject().get("url").getAsString(); //Get url to repository
+
+            switch(type){
+                case "PushEvent":
+                    System.out.println("\n-------------------------------");
+                    System.out.println("Event Type: "+type);
+                    System.out.println("Made commits to "+ repoUrl);
+                    System.out.println("-------------------------------");
+                    break;
+                case "WatchEvent":
+                    System.out.println("\n-------------------------------");
+                    System.out.println("Event Type: "+type);
+                    System.out.println("Starred repository: "+ repoUrl);
+                    System.out.println("-------------------------------");
+                    break;
+                case "CreateEvent":
+                    System.out.println("\n-------------------------------");
+                    System.out.println("Event Type: "+type);
+                    System.out.println("Git branch created: "+ repoUrl);
+                    System.out.println("-------------------------------");
+                    break;
+                case "DeleteEvent":
+                    System.out.println("\n-------------------------------");
+                    System.out.println("Event Type: "+type);
+                    System.out.println("Deleted Git branch: "+ repoUrl);
+                    System.out.println("-------------------------------");
+                    break;
+                default:
+                    System.out.println("\n-------------------------------");
+                    System.out.println("Event Type: "+type);
+                    System.out.println("GitHub Repository: "+repoUrl);
+                    System.out.println("-------------------------------");
+            }
         }
     }
 }
