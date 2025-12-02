b5f6d4f2a49cff7f27436c112c1ded1419c058a6
Ifeouwa Olujobi <ifeoluwaolujobi@gmail.com>
2025-12-02 05:32:39 +0100

    Made changes to get commits size

--- src/main/java/GitHubActivity.java
+++ src/main/java/GitHubActivity.java
@@ -8,14 +8,15 @@ public class GitHubActivity {
     static void main(String[] args) {
         String username = "olujobii";
 
-        if(args.length == 0){
-            System.out.println("No username is specified");
-            return;
-        }
+//        if(args.length == 0){
+//            System.out.println("No username is specified");
+//            return;
+//        }
+//
+//        if(args.length != 1){
+//            System.out.println("Wrong usage");
+//        }
 
-        if(args.length != 1){
-            System.out.println("Wrong usage");
-        }
         APIRepository apiRepository = new APIRepository(username);
         HttpResponse<String> httpResponse;
 
@@ -48,37 +49,36 @@ public class GitHubActivity {
         for(var elements : jsonArray){
             JsonObject jsonObject = elements.getAsJsonObject();
             String type = jsonObject.get("type").getAsString(); //Get type of event
-            String repoUrl = jsonObject.get("repo").getAsJsonObject().get("url").getAsString(); //Get url to repository
+            String repoName = jsonObject.get("repo").getAsJsonObject().get("name").getAsString(); //Get url to repository
+            JsonObject payload = jsonObject.get("payload").getAsJsonObject(); //Get payload
 
             switch(type){
                 case "PushEvent":
+                    int commit = payload.has("commits") ? payload.get("commits").getAsJsonArray().size() : 1;
+
                     System.out.println("\n-------------------------------");
-                    System.out.println("Event Type: "+type);
-                    System.out.println("Made commits to "+ repoUrl);
+                    System.out.println("Pushed "+commit +" commit(s) to "+repoName);
                     System.out.println("-------------------------------");
                     break;
                 case "WatchEvent":
                     System.out.println("\n-------------------------------");
-                    System.out.println("Event Type: "+type);
-                    System.out.println("Starred repository: "+ repoUrl);
+                    System.out.println("Starred "+repoName);
                     System.out.println("-------------------------------");
                     break;
                 case "CreateEvent":
                     System.out.println("\n-------------------------------");
-                    System.out.println("Event Type: "+type);
-                    System.out.println("Git branch created: "+ repoUrl);
+                    System.out.println("Created git branch at "+repoName);
                     System.out.println("-------------------------------");
                     break;
                 case "DeleteEvent":
                     System.out.println("\n-------------------------------");
-                    System.out.println("Event Type: "+type);
-                    System.out.println("Deleted Git branch: "+ repoUrl);
+                    System.out.println("Deleted git branch at "+repoName);
                     System.out.println("-------------------------------");
                     break;
                 default:
                     System.out.println("\n-------------------------------");
                     System.out.println("Event Type: "+type);
-                    System.out.println("GitHub Repository: "+repoUrl);
+                    System.out.println("GitHub Repository: "+repoName);
                     System.out.println("-------------------------------");
             }
         }
